cc3cc805b8efdde1177df5c4801192c3ce1764ae
Jose Helio <joseheliofvrb@gmail.com>
2022-04-19 12:49:10 -0300

    Revisto a coloração na checagem

--- script.js
+++ script.js
@@ -1,29 +1,91 @@
-let letra = document.getElementsByClassName("input")
-let pos = 0
+/*dispatchEvent*/
+
+const palavra = "tunel";
+
+let letra = elemento('input')
+var b = document.getElementsByTagName("svg");
+console.log(letra.length);
+let pos = 0;
+
+function elemento(nomeclasse) {
+  return document.getElementsByClassName(nomeclasse);
+}
 
 function adicionarbordalinha(linha) {
-    for (var i = 0; i < linha + 5 ; i++) {
-    letra[i].classList.add("border-edit")
-    console.log(i);
+  for (var i = 0; i < linha + 5; i++) {
+    letra[i].classList.add("border-edit");
+    // console.log(i);
+  }
+}
+
+function pegaTecla(event) {
+  var tecla = event.keyCode;
+
+  if (tecla > 65 || tecla < 90) {
+    console.log(tecla);
+
+    b[4].classList.add("dark");
+    b[4].classList.remove("light");
+    var letra = String.fromCharCode(tecla)
+    console.log('Aqui', letra);
+
+    if (alfabeto.indexOf(letra) != -1) {
+      return letra;
+    }
+    pos--;
+    return "";
+  }
+}
+
+
+function atribue(event) {
+  letra[pos].innerHTML = pegaTecla(event);
+  pos++;
+  console.log("pos", pos);
+  if (pos % 5 == 0) {
+    b[5].classList.add("dark");
+    b[5].classList.remove("light");
+  } else {
+    b[5].classList.add("light");
+    b[5].classList.remove("dark");
+
+  }
+
+}
+
+window.addEventListener("load", adicionarbordalinha(0));
+
+document.addEventListener("keydown", atribue);
+
+const alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+
+function checar() {
+
+
+  for (var i = 0; i < 5; i++) {
+    var l = letra[pos - 5 + i].innerHTML;
+
+
+    if (palavra.toUpperCase().indexOf(l) != -1) {
+      letra[pos - 5 + i].classList.add("letteryellow");
+    } else {
+      letra[pos - 5 + i].classList.add("letterred");
+    }
+
+    if (l == palavra[i].toUpperCase()) {
+      letra[pos - 5 + i].classList.add("lettergreem");
+      console.log(l, palavra[i].toUpperCase());
     }
     
+  }
+
+
+
+  adicionarbordalinha(pos)
 }
 
-window.addEventListener("load",adicionarbordalinha(0))
 
+var checagem = elemento('check')[1].addEventListener('click', checar)
 
-function pegaTecla() {
-    var tecla = event.keyCode;
-    return String.fromCharCode(tecla);
-}
-
-function atribue() {
-    letra[pos].innerHTML = pegaTecla()
-    pos ++;
-}
-
-document.addEventListener('keydown', atribue);
-
-const alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
-console.log(alfabeto);
+console.log(checagem);
