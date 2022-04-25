cc7dc1d2d25a4bbce9e6e6c8360a3cab92b55f43
Jose Helio <joseheliofvrb@gmail.com>
2022-04-25 13:02:51 -0300

    Adição da variavel basePalavras

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 /*dispatchEvent*/
+const basePalavras = ['antes','ambos','alvos','ajude','ajuda','ainda','agora','abril','braun','bordo','board','barra','baixa','autor','assim','armas','criar','craft','copia','conta','colin','citar','cinco','chris','drive','ditas','disse','delas','david','dados','crise','criou','estas','estar','estao','epoca','entre','entao','ended','earth','first','final','filme','ficou','feito','fazer','facts','estes','horas','homem','haven','guide','grupo','frase','foram','folha','julio','julho','jones','joint','james','ideia','houve','house','livro','livre','lider','levou','levar','laser','lados','junho','menos','marte','marco','maior','lunar','loves','longo','local','needs','naves','naval','natal','muito','moon»','mesmo','meses','ocean','obama','nunca','novas','notas','noite','neste','nesta','pelos','pecas','peace','passo','parte','pages','orbit','olhei','radio','quase','press','pouso','poole','poder','placa','pisar','santo','salvo','salto','russo','russa','roger','reiss','recem','small','simon','serio','serie','seria','seres','sendo','secao','terra','tendo','subiu','space','sonda','solar','sobre','smith','uniao','total','todos','tinha','times','texto','terre','verne','venus','usava','union'
+]
+const palavra = basePalavras[Math.round(Math.random() * (basePalavras.length-1))];
 
-const palavra = "tunel";
+console.log(palavra);
 
 function atrCorTitulo() {
   let arr = [];
